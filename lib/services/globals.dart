f4e21b3b9d10c51ddb8fa5deb1d2bb3e8d2624b4
Matías Infante <matias.infante@est.fi.uncoma.edu.ar>
2022-11-19 23:11:43 -0300

    add auth service and dependencies

---
+++ lib/services/globals.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const String baseURL = "http://192.168.0.6:8000/api/"; //emulator localhost
+const Map<String, String> headers = {"Content-Type": "application/json"};
+
+errorSnackBar(BuildContext context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    backgroundColor: Colors.red,
+    content: Text(text),
+    duration: const Duration(seconds: 1),
+  ));
+}
